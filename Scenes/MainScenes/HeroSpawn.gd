be831687211c55ae7d51882f56202176bc31c0ec
Christoph Sengestack <christoph@sengestack.de>
2022-06-08 08:54:10 +0200

    Can equip heroes and they spawn

--- Scenes/MainScenes/HeroSpawn.gd
+++ Scenes/MainScenes/HeroSpawn.gd
@@ -2,7 +2,16 @@ extends Node2D
 
 
 onready var spawn_positions: Array = self.get_children()
+onready var equipped_heroes: Array = []
 
 
 func _ready():
-	pass
+	spawn_heroes()
+
+
+func spawn_heroes():
+	for i in range(spawn_positions.size()):
+		if HeroInventoryManager.equips.has(i):
+			var selected_hero_name = HeroInventoryManager.equips[i][0]
+			var selected_hero = load("res://Scenes/Heroes/" + selected_hero_name + ".tscn").instance()
+			spawn_positions[i].add_child(selected_hero)
